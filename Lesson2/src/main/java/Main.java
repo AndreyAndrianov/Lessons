2c27ff83091873157c922712f02cd7176c717ddd
AnandreyAndrianov <andrey.andrianov2011@ya.ru>
2017-12-01 10:22:53 +0300

    Main updated

--- Lesson2/src/main/java/Main.java
+++ Lesson2/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         Item item = new Item("name", "Descr", 123L);
-        item = tracker.add(item);
-        System.out.println(item.getName());
+        tracker.add(item);
+        System.out.println(item.getId());
     }
 }
